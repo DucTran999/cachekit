1e36597c39ad1ac4fd5e6d63bae8d9bdb684030f
Đức Trần <tranaduc9x@gmail.com>
2025-06-23 22:45:50 +0700

    add example for redis cache

---
+++ examples/redis/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/DucTran999/cachekit"
+	"github.com/DucTran999/cachekit/config"
+)
+
+func main() {
+	cfg := config.RedisConfig{
+		Host:     "localhost",
+		Port:     6379,
+		Username: "default",
+		Password: "example",
+		DB:       1,
+	}
+
+	cache, err := cachekit.NewRedisCache(cfg)
+	if err != nil {
+		log.Fatalf("[FATAL] failed to connect redis: %v", err)
+	}
+
+	log.Println("[INFO] redis connected")
+	defer cache.Close()
+}
